65aead3b0ae5a36596f70b3be2230c5aa3ee9c7b
adityamandal-Developer <mandaladitya901@gmail.com>
2024-09-18 13:35:57 +0530

    lucky seven solved

--- lucky_sevens.js
+++ lucky_sevens.js
@@ -1,1 +1,20 @@
 // Write a function called lucky_sevens which takes an array of integers and returns true if any three consecutive elements sum to 7.
+
+const lucky_Seven = (arr) => {
+    if (arr.length < 3) {
+        return "Not possible"
+    }
+    for (i = 0; i < arr.length; i++) {
+        if ((arr[i] + arr[i + 1] + arr[i + 2]) === 7) {
+            console.log(arr[i], arr[i + 1], arr[i + 2] + " = 7");
+            return true
+        }
+
+    }
+    return false
+}
+
+
+
+const arr1 = [1, 3, 2, 0, 3, 4, 5, 9]
+lucky_Seven(arr1)
