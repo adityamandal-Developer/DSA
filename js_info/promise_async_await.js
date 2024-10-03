1147bf7696381ce646fb5048734fc61426f107dc
adityamandal-Developer <mandaladitya901@gmail.com>
2024-10-03 22:13:50 +0530

    changed structure

---
+++ js_info/promise_async_await.js
@@ -0,0 +1,29 @@
+function resolveAfter2Seconds() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // Expected output: "resolved"
+}
+
+asyncCall();
+
+
+// Promise.all()
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+});
+// Expected output: Array [3, 42, "foo"]
